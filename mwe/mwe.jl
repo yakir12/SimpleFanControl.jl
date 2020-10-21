5fba879ad584d79c312175b368c842921a28915a
Yakir Gagnon <12.yakir@gmail.com>
2020-10-21 15:46:18 +0200

    replace HTTP dependancy with using JSServe.HTTP

--- mwe/mwe.jl
+++ mwe/mwe.jl
@@ -1,6 +1,5 @@
 import LibSerialPort
 using AbstractPlotting, WGLMakie, JSServe
-import HTTP
-Base.readuntil(a::HTTP.ConnectionPool.Transaction, b::T, c::U) where {T<:Function, U<:Real} = JSServe.readuntil(a, b, c)
+Base.readuntil(a::JSServe.HTTP.ConnectionPool.Transaction, b::T, c::U) where {T<:Function, U<:Real} = JSServe.readuntil(a, b, c)
 handler(session, request) = scatter(rand(5))
 app = JSServe.Application(handler, "0.0.0.0", 8000)
